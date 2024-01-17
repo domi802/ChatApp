8c651243dc8274531ddbe15611b6afacf30146a5
Dominik Woźniak <64065747+domi802@users.noreply.github.com>
2024-01-17 23:37:18 +0100

    Making dataBase and print current user

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,9 +1,22 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names, must_be_immutable
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  //current user logged
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  // future tu fetch user details
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +25,35 @@ class ProfilePage extends StatelessWidget {
         title: Text("Profile"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: getUserDetails(),
+        builder: ((context, snapshot) {
+          //loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          //error
+          else if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          }
+          //data
+          else if (snapshot.hasData) {
+            //extract data
+            Map<String, dynamic>? user = snapshot.data!.data();
+            return Column(
+              children: [
+                Text(user!['email']),
+                Text(user['username']),
+              ],
+            );
+          } else {
+            return Text("No data");
+          }
+        }),
       ),
     );
   }
